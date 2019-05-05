c6839219014ec1d746cbe1cc91e1eba3c537c98e
Rothanak So <rothanak.hello@gmail.com>
2019-05-04 22:21:31 -0400

    Fix code style for next.js pages

---
+++ pages/_app.jsx
@@ -0,0 +1,35 @@
+// pages/_app.js
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Head>
+          <title>Syllabank</title>
+          <script
+            defer
+            src="https://use.fontawesome.com/releases/v5.6.3/js/all.js"
+            integrity="sha384-EIHISlAOj4zgYieurP0SdoiBYfGJKkgWedPHH4jCzpCXLmzVsw1ouK59MuUtP4a1"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
