ca109b693b961e33f4ba472b87b42854793bc2fe
Rothanak So <rothanak.hello@gmail.com>
2019-05-09 20:14:58 -0400

    Add script for loading initial data into Firebase

---
+++ scripts/courseLoader.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import { loadFirebase } from '../lib/db';
+import data from '../data/data.json';
+
+dotenv.config();
+
+const db = loadFirebase().firestore();
+
+data.forEach((obj) => {
+  db.collection('courses').add({
+    course: obj.course,
+    last_name: obj.last_name,
+    time_begin: obj.time_begin,
+    time_end: obj.time_end,
+    days: obj.days,
+    term: obj.term,
+    year: obj.year,
+  }).then((docRef) => {
+    console.log(`Document writtenw ith ID: ${docRef}`);
+  }).catch((error) => {
+    console.log(`Error adding document: ${error}`);
+  });
+});
