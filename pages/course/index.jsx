9c3701be6fb01b8aeaec99d22e49ed08f8747287
Rothanak So <rothanak.hello@gmail.com>
2019-08-07 23:03:39 -0400

    Redirect /courses to /

---
+++ pages/course/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import redirect from '../../lib/redirect';
+
+export default class extends React.Component {
+  static async getInitialProps(context) {
+    redirect(context, '/');
+    return {};
+  }
+}
