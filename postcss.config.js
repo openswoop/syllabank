853ce75df4458f80403617c7200e4159a886127d
Rothanak So <rothanak.hello@gmail.com>
2019-08-21 10:59:08 -0400

    Optimize CSS with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,27 @@
-// const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './pages/**/*.jsx',
+    './pages/**/*.tsx',
+    './components/**/*.jsx',
+    './components/**/*.tsx',
+  ],
+
+  // Whitelist selectors to stop purgecss from removing them
+  whitelist: ['html', 'body'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
   plugins: [
     require('postcss-easy-import'),
     require('tailwindcss'),
-    require('autoprefixer')
-  ]
-}
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : [],
+  ],
+};
