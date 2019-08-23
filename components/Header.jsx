d1c2000498be19517e871e7f30b80a2a14947882
Rothanak So <rothanak.hello@gmail.com>
2019-08-23 16:51:55 -0400

    Link logo to home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,8 +5,8 @@ import Search from './Search';
 const Nav = () => (
   <div className="flex justify-between">
     <div className="logo-container">
-      <span className="logo">UNF</span>
-      <span className="app-title">Syllabus Bank</span>
+      <span className="logo"><a href="/">UNF</a></span>
+      <span className="app-title"><a href="/">Syllabus Bank</a></span>
       <span className="bg-white opacity-25 shadow-sm text-blue-800 px-3 sm:px-5 py-1 rounded-full ml-3 text-xs font-bold">Dev Build</span>
     </div>
     <div className="action-container">
