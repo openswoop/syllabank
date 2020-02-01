e318d9800061df1c837e4bb30304d6a78c7ff15f
Rothanak So <rothanak.hello@gmail.com>
2020-02-01 17:35:44 -0500

    Add cards to home page to help exploration

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import posed, { PoseGroup } from 'react-pose';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDropbox } from '@fortawesome/free-brands-svg-icons';
@@ -73,6 +74,28 @@ const ResultsBody = ({ results }) => (
   </Container>
 );
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const HomeCard = ({ course }) => (
+  <div
+    className="relative flex-1 p-5 rounded-lg mx-2 bg-white mb-4 sm:mb-0"
+    style={{
+      boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.025), 0px 0px 15px rgba(0, 0, 0, 0.05)',
+    }}
+  >
+    <div className="absolute -mt-6 mx-auto h-6 rounded-lg bg-gray-200" style={{ left: 10, right: 10, zIndex: -1 }} />
+    <div className="flex items-center justify-between">
+      <span className="text-blue-800 font-medium">{course.name}</span>
+      {/* <span className="font-light text-gray-500 text-sm">1 day ago</span> */}
+    </div>
+    <div className="mt-1 mb-4"><span className="font-light text-gray-800">{course.code}</span></div>
+    <Link href="/course/[id]" as={`/course/${course.code}`}>
+      <div className="rounded-lg cursor-pointer px-4 py-2 shadow bg-blue-700 text-white text-sm font-medium text-center">
+        {course.syllabi} Syllabi
+      </div>
+    </Link>
+  </div>
+);
+
 const Content = ({ isVisible, results, emptyMessage }) => {
   let content = null;
   if (isVisible) {
@@ -87,6 +110,26 @@ const Content = ({ isVisible, results, emptyMessage }) => {
         <FadeInOut key="results">
           <ResultsBar numResults={results.length} />
           <ResultsBody results={results} />
+        </FadeInOut>
+      );
+    } else {
+      content = (
+        <FadeInOut key="explore">
+          <Container>
+            <div className="w-4/5 sm:w-3/5 ml-auto mr-auto mt-10">
+              <div className="py-6 mb-2 text-center text-gray-500">
+                Don&apos;t have a specific course in mind? <span className="font-medium">Explore these:</span>
+              </div>
+              <div className="sm:flex">
+                <HomeCard course={{ name: 'Programming I', code: 'COP2220', syllabi: 10 }} />
+                <HomeCard course={{ name: 'Programming II', code: 'COP3503', syllabi: 2 }} />
+                <HomeCard course={{ name: 'Data Structures', code: 'COP3530', syllabi: 2 }} />
+              </div>
+              <div className="text-right mt-8 text-gray-300 font-mono text-sm">
+                made by <a className="underline hover:text-gray-600" href="https://twitter.com/howardunfduck" target="_blank" rel="noopener noreferrer">howard the duck</a> ♥
+              </div>
+            </div>
+          </Container>
         </FadeInOut>
       );
     }
