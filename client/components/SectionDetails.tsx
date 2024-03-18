d3e3e2d31e9fe72614741e15dd49cc4ad3d7b861
Andreas Ink <andreasink17@gmail.com>
2024-03-18 18:22:18 -0400

    adds rank col

--- client/components/SectionDetails.tsx
+++ client/components/SectionDetails.tsx
@@ -26,8 +26,9 @@ export const SectionDetails: React.FC<Props> = ({ course }) => (
         <div className={classNames(HEADER_CN, 'w-full sm:w-1/2')}>Professor</div>
         <div className={classNames(HEADER_CN, 'hidden sm:block sm:w-1/2')}>Time</div>
       </div>
-      <div className="flex flex-1">
+      <div className="flex flex-1 sm:flex-2">
         <div className={classNames(HEADER_CN, 'w-full')}>Syllabus</div>
+        <div className={classNames(HEADER_CN, 'w-full')}>Rank</div>
       </div>
     </div>
 
@@ -48,8 +49,8 @@ export const SectionDetails: React.FC<Props> = ({ course }) => (
               : `${section.days} ${section.time_begin && toFormattedTime(section.time_begin)}`}
           </div>
         </div>
-        <div className="flex flex-1 truncate">
-          <div className={classNames(CELL_CN, 'w-full text-center')}>
+        <div className="flex-1 sm:flex sm:flex-2 truncate">
+          <div className={classNames(CELL_CN, 'sm:w-1/2 text-leading')}>
             {section.syllabus ? (
               <a
                 href={resolvePublicUrl(section.syllabus)}
@@ -58,12 +59,15 @@ export const SectionDetails: React.FC<Props> = ({ course }) => (
                 className="text-blue-800 no-underline"
               >
                 <Book className="fill-current inline mr-1" style={{ width: 16, height: 16 }} />
-                Open PDF
+                PDF
               </a>
             ) : (
               <span className="text-gray-500">-</span>
             )}
           </div>
+          <div className={classNames(CELL_CN, 'sm:w-1/2 text-leading')}>
+          <span className="text-gray-500">-</span>
+            </div>
         </div>
       </div>
     ))}
